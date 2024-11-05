8a21076a371023ed45213a2769089ac4ac5cd702
pedebr <peder.brennum@gmail.com>
2024-11-05 12:43:30 +0100

    initial commit

---
+++ hotel/src/test/java/com/peder/hotel/TestHotelApplication.java
@@ -0,0 +1,11 @@
+package com.peder.hotel;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(HotelApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
